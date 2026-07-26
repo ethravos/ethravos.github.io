783fa95c86b6ea39f195db484e449cbee42829ca
Latent Architect <latentarchitect@gmail.com>
2026-07-26 15:09:51 -0400

    mobile essentially fixed

--- docs/scripts/nav.js
+++ docs/scripts/nav.js
@@ -1,6 +1,8 @@
 const hamburgerMenu = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav__list");
+const navMenu = document.querySelector(".nav-menu");
 
 hamburgerMenu.addEventListener("click", () => {
-    nav.classList.toggle("active")
+    nav.classList.toggle("active");
+    navMenu.classList.toggle("active");
 });
